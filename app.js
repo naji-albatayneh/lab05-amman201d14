5556968b9d3103e84bce4655495e8f279d472880
Jane Smith <example@email.com>
2021-02-14 13:26:34 +0800

    completing strech goal problem6 multipyAnyArray

--- app.js
+++ app.js
@@ -145,9 +145,15 @@ let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) {
   //eslint-disable-line
+  let m = 1;
+  for (let i =0; i<dynamicArray.length;i++){
+    m=multiply(m, dynamicArray[i])[0];
+    //console.log(m);
+  }
+  return [m, `The numbers ${dynamicArray} have a product of ${m}.`]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
